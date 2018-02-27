f93aafeccdd5ca3d857182435f585de068a10c77
NaokiKenmochi <kemmochi.naoki.62r@st.kyoto-u.ac.jp>
2018-02-27 09:55:29 +0900

    リリース前のコミット

--- RT1DataBrowser.py
+++ RT1DataBrowser.py
@@ -1,12 +1,7 @@
 import read_wvf
 import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
 import numpy as np
-import matplotlib.cbook as cbook
-import matplotlib.image as image
-from PIL import Image
 import scipy.signal as sig
-from scipy import optimize
 
 
 class DataBrowser:
@@ -106,6 +101,11 @@ class DataBrowser:
 
     def load_date(self, LOCALorPPL):
         """
+        LOCALorPPL == "PPL"の場合:
+            exp_ep01, exp_ep01から実験データをロードします
+
+        LOCALorPPL == "LOCAL"の場合:
+            ローカルに保存してあるデータを読み込みます
 
         :param LOCALorPPL:
         :return:
@@ -117,11 +117,11 @@ class DataBrowser:
             data_ep01 = dm_ep01.fetch_raw_data(self.shotnum)
             data_ep02_MP = dm_ep02_MP.fetch_raw_data(self.shotnum)
             data_ep02_SX = dm_ep02_SX.fetch_raw_data(self.shotnum)
-            #np.savez("data_%s_%d" % (self.data, self.shotnum), data_ep01=data_ep01, data_ep02_MP=data_ep02_MP, data_ep02_SX=data_ep02_SX)
+            #np.savez_compressed("data/data_%s_%d" % (self.date, self.shotnum), data_ep01=data_ep01, data_ep02_MP=data_ep02_MP, data_ep02_SX=data_ep02_SX)
             print("Load data from PPL")
 
         else:
-            data = np.load("data_%s_%d.npz" % (self.date, self.shotnum))
+            data = np.load("data/data_%s_%d.npz" % (self.date, self.shotnum))
             data_ep01 = data["data_ep01"]
             data_ep02_MP = data["data_ep02_MP"]
             data_ep02_SX = data["data_ep02_SX"]
@@ -131,6 +131,7 @@ class DataBrowser:
 
     def multiplot(self):
         """
+        exp_ep01, exp_ep02に保存してあるRT-1の実験データを全て描写します
 
         :return:
         """
@@ -139,18 +140,6 @@ class DataBrowser:
         data_ep01 = self.adj_gain(data_ep01)
         data_ep01 = self.mag_loop(data_ep01)
         data_ep01 = self.calib_IF(data_ep01)
-        time_ep02_SX = np.arange(0,2,2/2000000)
-        time_ep02_MP = np.arange(0,2,2/1000000)
-
-#        #datafile = cbook.get_sample_data("LOGO_en_140.jpg", asfileobj=False)
-#        #im = image.imread("LOGO_en_140.jpg")
-#        im = image.imread("LOGO.png")
-#        #im = np.array(im)
-#        ax1 = fig.add_subplot(5,2,1, sharex=None, sharey=None)
-#        ax1.imshow(im, aspect='auto', extent=(0.5,1.1,0.0,0.1), alpha=1.0, zorder=-1)
-#        newax = fig.add_axes([0.0, 0.8, 0.1, 0.2], anchor='NE', zorder=-1)
-#        newax.imshow(im)
-#        newax.axis('off')
 
         #############################
         #   Date in exp_ep01        #
@@ -169,7 +158,8 @@ class DataBrowser:
         #############################
         for j in range(1,5):
             ax1 = fig.add_subplot(6,2, int(self.data_pos_name_ep02[j+3,0]), sharex=None, sharey=None)
-            ax1.plot(data_ep02_SX[0,::20*self.num_step]+0.5,data_ep02_SX[j,::20*self.num_step]+0.1-0.10*j, label=self.data_pos_name_ep02[j+3, 1])
+            ax1.set_xlim(0.5, 2.5)
+            ax1.plot(data_ep02_SX[0,::20*self.num_step]+1.25,data_ep02_SX[j,::20*self.num_step]+0.1-0.10*j, label=self.data_pos_name_ep02[j+3, 1])
             plt.subplots_adjust(left=0.05, right=0.97, bottom=0.05, top=0.95, wspace=0.15, hspace=0.15)
             ax1.legend(fontsize=10)
 
@@ -178,8 +168,9 @@ class DataBrowser:
         #############################
         for j in range(1,5):
             ax1 = fig.add_subplot(6,2, int(self.data_pos_name_ep02[j-1,0]), sharex=None, sharey=None)
+            ax1.set_xlim(0.5, 2.5)
             #ax1.plot(data_ep02_MP[0,::20*self.num_step]+0.5,data_ep02_MP[j,::20*self.num_step]+0.2-0.10*j, label=self.data_pos_name_ep02[j-1, 1])
-            ax1.plot(data_ep02_MP[0,::self.num_step]+0.5,data_ep02_MP[j,::self.num_step]+0.2-0.10*j, label=self.data_pos_name_ep02[j-1, 1])
+            ax1.plot(data_ep02_MP[0,::self.num_step]+1.25,data_ep02_MP[j,::self.num_step]+0.2-0.10*j, label=self.data_pos_name_ep02[j-1, 1])
             #ax1.plot(time_ep02_MP[::100], data_ep02_MP[j,::100]+0.5-0.25*j, label=self.data_name_ep02[j-1])
             plt.subplots_adjust(left=0.05, right=0.97, bottom=0.05, top=0.95, wspace=0.15, hspace=0.15)
             ax1.legend(fontsize=10)
@@ -187,9 +178,9 @@ class DataBrowser:
                 plt.title("Date: %s, Shot No.: %d" % (self.date, self.shotnum), loc='right', fontsize=36, fontname="Times New Roman")
 
         ax1 = fig.add_subplot(6,2,4)
-        self.stft(data_ep02_MP[0,:], data_ep02_MP[3,:], self.data_pos_name_ep02[2,1], nperseg=512, vmax=1e-4)
+        self.stft(data_ep02_MP[0,:], data_ep02_MP[3,:], self.data_pos_name_ep02[2,1], nperseg=512, vmax=1e-4, time_offset=0.25)
         ax1 = fig.add_subplot(6,2,8)
-        self.stft(data_ep02_SX[0,:], data_ep02_SX[2,:], self.data_pos_name_ep02[4,1], nperseg=25000, vmax=8e-4)
+        self.stft(data_ep02_SX[0,:], data_ep02_SX[2,:], self.data_pos_name_ep02[4,1], nperseg=25000, vmax=8e-4, time_offset=0.75)
         filepath = "figure/"
         filename = "RT1_%s_%d" % (self.date, self.shotnum)
         plt.savefig(filepath + filename)
@@ -198,6 +189,7 @@ class DataBrowser:
 
     def mag_loop(self, ml):
         """
+        磁気ループの信号を反磁性信号（単位はWb）に較正します
 
         :param ml:
         :return:
@@ -217,14 +209,12 @@ class DataBrowser:
 
     def calib_IF(self, IF):
         """
+        マイクロ波干渉計の位相差信号を，密度の値に較正します
+        （フリンジジャンプの補正は未実装）
 
         :param IF:
         :return:
         """
-        np.savetxt("IF_20170608_74_raw.txt", IF[11:13, :].T, delimiter=",")
-        #np.savetxt("IF2_20170629_68.txt", IF[11, :], delimiter="\n")
-        #np.savetxt("IF1_20170629_68.txt", IF[10, :], delimiter="\n")
-
         IF_offset = np.mean(np.arcsin((IF[10,:5000]-self.a1)/self.b1)*180/np.pi)
         #IF[10,:] = np.arcsin((IF[10,:]-self.a1)/self.b1)*180/np.pi - np.mean(np.arcsin((IF[10,:5000]-self.a1)/self.b1)*180/np.pi)
         IF[10,:] = np.arcsin((IF[10,:]-self.a1)/self.b1)*180/np.pi
@@ -236,13 +226,6 @@ class DataBrowser:
         IF[10,:] = IF[10,:]*5.58/360
         IF[11,:] = IF[11,:]*5.58/360
         IF[12,:] = IF[12,:]*5.58/360
-#        IF[10,:] = np.arcsin((IF[10,:]-self.a1)/self.b1)*180/np.pi
-#        IF[11,:] = np.arcsin((IF[11,:]-self.a2)/self.b2)*180/np.pi
-#        IF[12,:] = np.arcsin((IF[12,:]-self.a3)/self.b3)*180/np.pi
-#
-#        IF[10,:] -= np.mean(IF[10,:6000])
-#        IF[11,:] -= np.mean(IF[11,:6000])
-#        IF[12,:] -= np.mean(IF[12,:6000])
 
         return IF
 
@@ -271,123 +254,30 @@ class DataBrowser:
 
         return data_ep01
 
-    def stft(self, x, y, label, nperseg, vmax):
+    def stft(self, x, y, label, nperseg, vmax, time_offset):
+        """
+        短時間フーリエ変換を行います
+        :param x:
+        :param y:
+        :param label:
+        :param nperseg:
+        :param vmax:
+        :param time_offset:
+        :return:
+        """
         MAXFREQ = 1e0
         N = 1e-3*np.abs(1/(x[1]-x[2]))
         f, t, Zxx =sig.spectrogram(y, fs=N, window='hamming', nperseg=nperseg)
-        #plt.xlim(0, 1.0)
-        plt.pcolormesh(t, f, np.abs(Zxx), vmin=0, vmax=vmax)
+        plt.pcolormesh(t*1e-3+time_offset, f, np.abs(Zxx), vmin=0, vmax=vmax)
         #plt.contourf(t, f, np.abs(Zxx), 200, norm=LogNorm())# vmax=1e-7)
         plt.ylabel(label + "\nFrequency [kHz]")
         plt.ylim([0, MAXFREQ])
+        plt.xlim([0.5, 2.5])
 
-    def fit_func(self, parameter, x, y, t0):
-        y0 = parameter[0]
-        A = parameter[1]
-        tau = parameter[2]
-        x0 = t0#parameter[3]
-        #exp_XOffset = y - (y0 + A*np.exp(-(x-x0)/tau))
-        exp_XOffset = y - A*np.exp(-(x-x0)/tau)
-        return exp_XOffset
-
-    def get_tau(self, x, y, t0):
-        parameter0 = [0., 0., 0.1, 0.]
-        result = optimize.leastsq(self.fit_func, parameter0, args=(x, y, t0), maxfev=5000)
-        y0_fit = 0#result[0][0]
-        A_fit = result[0][1]
-        tau_fit = result[0][2]
-        x0_fit = t0#result[0][3]
-        print(self.shotnum, y0_fit, A_fit, tau_fit, x0_fit)
-        #plt.plot(x, y)
-        plt.plot(x, y0_fit + A_fit*np.exp(-(x-x0_fit)/tau_fit))
-        plt.text(0.9, 0.8*x0_fit, "y0 + A*np.exp(-(x-x0)/tau)", fontsize=12)
-        plt.text(0.9, 0.6*x0_fit, "y0=%f" % y0_fit, fontsize=12)
-        plt.text(0.9, 0.4*x0_fit, "A=%f" % A_fit, fontsize=12)
-        plt.text(0.9, 0.2*x0_fit, "tau=%f" % tau_fit, fontsize=12)
-        plt.text(0.9, 0.0*x0_fit, "x0=%f" % x0_fit, fontsize=12)
-        plt.xlabel("Time[sec]", fontsize=18)
-        plt.ylabel("$\mathbf{n_eL [10^{17}m^{-2}]}$", fontsize=18)
-        plt.tick_params(labelsize=18)
-
-    def plt_IFwfit(self, LOCALorPPL, pltstart, graph="SHOW_GRAPH"):
-        fig = plt.figure(figsize=(12,8))
-
-        if LOCALorPPL == "PPL":
-            dm_ep01 = read_wvf.DataManager("exp_ep01", 0, self.date)
-            data_ep01 = dm_ep01.fetch_raw_data(self.shotnum)
-            #np.savez("data_%s_%d" % (self.data, self.shotnum), data_ep01=data_ep01, data_ep02_MP=data_ep02_MP, data_ep02_SX=data_ep02_SX)
-            print("Load data from PPL")
-
-        else:
-            data = np.load("data_%s_%d.npz" % (self.date, self.shotnum))
-            data_ep01 = data["data_ep01"]
-            print("Load data from local")
-
-        data_ep01 = self.adj_gain(data_ep01)
-        data_ep01 = self.calib_IF(data_ep01)
-        data_ep01 = self.mag_loop(data_ep01)
-
-        if(graph == "SHOW_GRAPH"):
-            ax1=plt.subplot(311)
-            pltnum = 10
-            plt.plot(data_ep01[0, 10000:22000:self.num_step], data_ep01[pltnum, 10000:22000:self.num_step], label=self.data_pos_name_ep01[pltnum,1])
-            plt.legend()
-            self.get_tau(data_ep01[0, pltstart:18000:self.num_step], data_ep01[pltnum, pltstart:18000:self.num_step], pltstart/10000)
-            plt.title("Date: %s, Shot No.: %d" % (self.date, self.shotnum), loc='right', fontsize=20, fontname="Times New Roman")
-            ax1=plt.subplot(312)
-            pltnum = 11
-            plt.plot(data_ep01[0, 10000:22000:self.num_step], data_ep01[pltnum, 10000:22000:self.num_step], label=self.data_pos_name_ep01[pltnum,1])
-            plt.legend()
-            self.get_tau(data_ep01[0, pltstart:18000:self.num_step], data_ep01[pltnum, pltstart:18000:self.num_step], pltstart/10000)
-            ax1=plt.subplot(313)
-            pltnum = 12
-            plt.plot(data_ep01[0, 10000:22000:self.num_step], data_ep01[pltnum, 10000:22000:self.num_step], label=self.data_pos_name_ep01[pltnum,1])
-            plt.legend()
-            self.get_tau(data_ep01[0, pltstart:18000:self.num_step], data_ep01[pltnum, pltstart:18000:self.num_step], pltstart/10000)
-
-            filepath = "figure/"
-            filename = "GP1_%s_%d_IF1_IF2_IF3_y0" % (self.date, self.shotnum)
-            plt.savefig(filepath + filename)
-            plt.clf()
-
-        #filename2 = "GP1_%s_%d_IF1IF2IF3_all.txt" % (self.data, self.shotnum)
-        #filename3 = "GP1_%s_%d_ml3.txt" % (self.data, self.shotnum)
-        #filename4 = "GP1_%s_%d_2GPf.txt" % (self.data, self.shotnum)
-        filename5 = "GP1_%s_%d_IF1IF2FAST.txt" % (self.date, self.shotnum)
-        #IF1IF2IF3 = np.zeros((len(data_ep01[0]), 4))
-        #IF1IF2IF3[:, :] = data_ep01[9:13, :].T
-        #np.savetxt(filename2, IF1IF2IF3, delimiter=",")
-        #np.savetxt(filename3, data_ep01[19, :].T, delimiter=",")
-        #np.savetxt(filename4, data_ep01[3, :].T, delimiter=",")
-        np.savetxt(filename5, data_ep02[3, :].T, delimiter=",")
-
-
-    def load_FAST(self, LOCALorPPL):
-
-        if LOCALorPPL == "PPL":
-            #dm_ep02_SX = read_wvf.DataManager("exp_ep02", "SX", self.data)
-            #data_ep02_SX = dm_ep02_SX.fetch_raw_data(self.shotnum)
-            #np.savez("data_%s_%d" % (self.data, self.shotnum), data_ep01=data_ep01, data_ep02_MP=data_ep02_MP, data_ep02_SX=data_ep02_SX)
-            dm_ep02_MP = read_wvf.DataManager("exp_e02", "MP", self.date)
-            data_ep02_MP = dm_ep02_MP.fetch_raw_data(self.shotnum)
-            print("Load data from PPL")
-
-        else:
-            data = np.load("data_%s_%d.npz" % (self.date, self.shotnum))
-            data_ep02 = data["data_ep02"]
-            print("Load data from local")
-
-
-        filename5 = "GP1_%s_%d_IF1IF2FAST.txt" % (self.date, self.shotnum)
-        filename6 = "GP1_%s_%d_MP123FAST.txt" % (self.date, self.shotnum)
-        np.savez("MP123_%s_%d" % (self.date, self.shotnum), data_ep02_MP=data_ep02_MP)
-        #np.savetxt(filename5, data_ep02_SX[1:3, :].T, delimiter=",")
 
 if __name__ == "__main__":
-#    for i in range(107, 108):
-#        db = DataBrowser(data="20170608", shotNo=i, LOCALorPPL="PPL")
-#        db.plt_IFwfit(LOCALorPPL="PPL", pltstart=11200)
-#        db.load_FAST(LOCALorPPL="PPL")
-    db = DataBrowser(date="20180222", shotNo=66, LOCALorPPL="PPL")
+#    for i in range(47, 103):
+#        db = DataBrowser(date="20180223", shotNo=i, LOCALorPPL="PPL")
+#        db.load_date(LOCALorPPL="PPL")
+    db = DataBrowser(date="20180223", shotNo=101, LOCALorPPL="LOCAL")
     db.multiplot()
-#    db.plt_IFwfit(LOCALorPPL="PPL", pltstart=12200)
---
+++ STFT_RT1.py
@@ -0,0 +1,325 @@
+from RT1DataBrowser import DataBrowser
+from matplotlib import gridspec
+import sys
+sys.path.append('/Users/kemmochi/PycharmProjects/ControlCosmoZ')
+
+import numpy as np
+import pywt
+import read_wvf
+import czdec
+import scipy.signal as sig
+import matplotlib.pyplot as plt
+import matplotlib.ticker
+from matplotlib.colors import LogNorm
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib as mpl
+#mpl.use('Qt4Agg')
+
+
+class STFT_RT1(DataBrowser):
+    def __init__(self, date, shotNo, LOCALorPPL):
+        """
+
+        :param date:
+        :param shotNo:
+        """
+        super().__init__(date, shotNo, LOCALorPPL)
+        self.date = date
+        self.shotnum = shotNo
+        self.LOCALorPPL = LOCALorPPL
+
+    def load_ep01(self, LOCALorPPL):
+        if LOCALorPPL == "PPL":
+            dm_ep01 = read_wvf.DataManager("exp_ep01", 0, self.date)
+            data_ep01 = dm_ep01.fetch_raw_data(self.shotnum)
+            print("Load ep01 from PPL")
+
+        else:
+            data = np.load("IF_%s_%d.npz" % (self.date, self.shotnum))
+            data_ep02_SX = data["data_ep02_SX"]
+            filename = "GP1_20171110_107_IF1IF2FAST.txt"
+            IF_FAST = np.loadtxt(filename, delimiter=",")
+            print("Load SX from local")
+
+        return data_ep01
+
+    def load_IF_FAST(self, LOCALorPPL):
+        if LOCALorPPL == "PPL":
+            dm_ep02_SX = read_wvf.DataManager("exp_ep02", "SX", self.date)
+            data_ep02_SX = dm_ep02_SX.fetch_raw_data(self.shotnum)
+            print("Load SX from PPL")
+
+        else:
+            data = np.load("IF_%s_%d.npz" % (self.date, self.shotnum))
+            data_ep02_SX = data["data_ep02_SX"]
+            filename = "GP1_20171110_107_IF1IF2FAST.txt"
+            IF_FAST = np.loadtxt(filename, delimiter=",")
+            print("Load SX from local")
+
+        return data_ep02_SX
+
+    def load_MP_FAST(self, LOCALorPPL):
+        if LOCALorPPL == "PPL":
+            dm_ep02_MP = read_wvf.DataManager("exp_ep02", "MP", self.date)
+            data_ep02_MP = dm_ep02_MP.fetch_raw_data(self.shotnum)
+            print("Load MP from PPL")
+
+        else:
+            data = np.load("MP123_%s_%d.npz" % (self.date, self.shotnum))
+            data_ep02_MP = data["data_ep02_MP"]
+            print("Load MP from local")
+
+        return data_ep02_MP
+
+    def load_SX_FAST(self, LOCALorPPL):
+        if LOCALorPPL == "PPL":
+            dm_ep02_SX = read_wvf.DataManager("exp_ep02", "SX", self.date)
+            data_ep02_SX = dm_ep02_SX.fetch_raw_data(self.shotnum)
+            print("Load SX from PPL")
+
+        else:
+            data = np.load("SX123_%s_%d.npz" % (self.date, self.shotnum))
+            data_ep02_MP = data["data_ep02_SX"]
+            print("Load SX from local")
+
+        return data_ep02_SX
+
+    def load_SX_CosmoZ(self, LOCALorPPL):
+        if LOCALorPPL == "PPL":
+            data = czdec.CosmoZ_DataBrowser(filepath= '/Volumes/share/Cosmo_Z_xray/', filename="", date=self.date, shotnum=self.shotnum)
+            data_SX = data.read_cosmoz()
+            print("Load SX from PPL")
+
+        else:
+            #data = np.load("/Users/kemmochi/PycharmProjects/ControlCosmoZ/SX_%s_%d.npz" % (self.date, self.shotnum))
+            data = np.load("data/19.npz")
+            data_SX = data['energy']
+            time_SX = data['time']
+            print("Load SX from local")
+
+        return data_SX, time_SX
+
+    def cwt(self):
+        # TODO: 時間がかかりすぎる　要確認
+        #IF_FAST = self.load_IF_FAST("PPL")
+        #MP_FAST = self.load_MP_FAST("PPL")
+        #y = MP_FAST[1, :]
+        #x = MP_FAST[0, :]
+        num_IF = 1
+        y = IF_FAST[num_IF, ::1000]
+        x = np.linspace(0, 2, 2000)
+        #N = 1e-3*np.abs(1/(x[1]-x[2]))
+        N=4000
+        wfreq = np.linspace(1,N, 4000)
+        #coef = sig.cwt(y, sig.ricker, wfreq)
+        #coef, freqs=pywt.cwt(y, wfreq, 'cmor')
+        coef, freqs=pywt.cwt(y, wfreq, 'mexh')
+
+        MAXFREQ = 1e0
+        #plt.xlim(0, 1.0)
+        #plt.contourf(t, f, np.abs(Zxx), 200, norm=LogNorm())# vmax=1e-7)
+        plt.ylabel("CWT Frequency of IF%d [Hz]" % (num_IF))
+        plt.xlabel("Time [sec]")
+        #plt.ylim([0, MAXFREQ])
+        #plt.xlim([0.8, 2.2])
+        #ax3.contourf(x, 200/wfreq, coef, 200)
+        plt.contourf(x,N*freqs, np.sqrt(np.real(coef)**2+np.imag(coef)**2), 100, vmax=5.0)
+        #ax3.contourf(x, 200/wfreq, np.sqrt(np.real(coef)**2+np.imag(coef)**2), 200, vmax=0.4)
+        #plt.colorbar()
+        plt.title("Date: %s, Shot No.: %d" % (self.date, self.shotnum), loc='right', fontsize=20, fontname="Times New Roman")
+        plt.show()
+
+    def moving_average(self, x, N):
+        # Take a moving average along axis=1 with window width N.
+        x = np.pad(x, ((0, 0), (N, 0)), mode='constant')
+        cumsum = np.cumsum(x, axis=1)
+        return (cumsum[:, N:] - cumsum[:, :-N]) / N
+
+    def cross_spectrum(self):
+        data_ep01 = self.load_ep01("PPL")
+        data_ep01 = self.adj_gain(data_ep01)
+        data_ep01 = self.calib_IF(data_ep01)
+        IF = data_ep01[9:12:2, :].T
+        N = np.abs(1/(data_ep01[0, 1]-data_ep01[0, 2]))
+        sampling_time = 1/N
+
+        #IF_FAST = self.load_IF_FAST("PPL")
+        #IF = IF_FAST[1:3, :].T
+        #sampling_time = 1e-6
+        #f, t, Pxx = sig.spectrogram(IF, axis=0, fs=1/sampling_time, window='hamming', nperseg=128, noverlap=64, mode='complex')
+        #f, t, Pxx = sig.spectrogram(IF, axis=0, fs=1/sampling_time, window='hamming', nperseg=2**15, noverlap=512, mode='complex')
+        f, t, Pxx = sig.spectrogram(IF, axis=0, fs=1/sampling_time, window='hamming', nperseg=2**9, noverlap=16, mode='complex')
+        #Pxx_run = self.moving_average(Pxx[:, 0] * np.conj(Pxx[:, 1]), 8)
+        Pxx_run = self.moving_average(Pxx[:, 0] * np.conj(Pxx[:, 1]), 2)
+
+        DPhase = 180/np.pi*np.arctan2(Pxx_run.imag, Pxx_run.real)
+
+        plt.pcolormesh(t, f, np.log(np.abs(Pxx_run)))
+        #plt.pcolormesh(t, f, DPhase)
+        plt.xlim(0.5, 2.5)
+        #plt.clim(-16, -13)
+        plt.clim(-28, -25)
+        plt.ylim(0, 2000)
+        plt.colorbar()
+        plt.xlabel('Time [sec]')
+        plt.ylabel('Frequency [Hz]')
+        plt.show()
+
+    def stft(self, IForMPorSX="IF", num_ch=1):
+
+        time_offset_stft = 0.0
+        if(IForMPorSX=="IF"):
+            data_ep01 = self.load_ep01("PPL")
+            data_ep01 = self.adj_gain(data_ep01)
+            data_ep01 = self.calib_IF(data_ep01)
+
+            y = data_ep01[10:13, :]
+            x = data_ep01[0, :]
+            filename = "STFT_IF_%s_%d" % (self.date, self.shotnum)
+            vmin = 0.0
+            vmax = 1e-5
+            NPERSEG = 2**9
+            time_offset = 0.0
+
+        if(IForMPorSX=="POL"):
+            """
+            390nm, 730nm, 710nm, 450nmの順で格納
+            390nm, 450nmの比を用いて電子密度を計算
+            730nm, 710nmの比を用いて電子温度を計算
+            """
+            data_ep01 = self.load_ep01("PPL")
+            data_ep01 = self.adj_gain(data_ep01)
+
+            y_buf1 = np.array([data_ep01[13, :]])
+            y_buf2 = np.array(data_ep01[25:28, :])
+            y = np.r_[y_buf1, y_buf2]
+            x = data_ep01[0, :]
+            filename = "STFT_POL_%s_%d" % (self.date, self.shotnum)
+            vmin = 0.0
+            vmax = 3e-7
+            NPERSEG = 2**9
+            time_offset = 0.0
+
+        if(IForMPorSX=="IF_FAST"):
+            IF_FAST = self.load_IF_FAST("PPL")
+            y = IF_FAST[1:, :]
+            x = np.linspace(0, 2, 2000000)
+            filename = "STFT_IF_FAST_%s_%d" % (self.date, self.shotnum)
+            vmin = 0.0
+            vmax = 5e-7
+            NPERSEG = 2**15
+            time_offset = 0.75
+            time_offset_stft = 0.75
+
+        if(IForMPorSX=="MP"):
+            MP_FAST = self.load_MP_FAST("PPL")
+            y = MP_FAST[1:, :]
+            x = MP_FAST[0, :]
+            filename = "STFT_MP_%s_%d" % (self.date, self.shotnum)
+            vmin = 0.0
+            vmax = 1e-7
+            NPERSEG = 1024
+            time_offset = 1.25
+            time_offset_stft = 0.25
+            #plt.plot(x, MP_FAST[1, :]+1, label="MP1")
+            #plt.plot(x, MP_FAST[2, :], label="MP2")
+            #plt.plot(x, MP_FAST[3, :]-1, label="MP3")
+            #plt.legend()
+            #plt.show()
+
+        if(IForMPorSX=="SX"):
+            data_SX, time_SX = self.load_SX_CosmoZ(self.LOCALorPPL)
+            y = data_SX[:, 4]
+            x = data_SX[:, 0]
+            time_SX_10M = np.linspace(0, 2, 2e7)
+            data_SX_10M = np.zeros(2e7)
+            data_SX_10M[[i for i in time_SX*1e7]] = data_SX
+            y = data_SX_10M
+            x = time_SX_10M
+            time_offset_stft = 1.00
+            time_offset = 1.25
+            #plt.plot(x, y)
+            #plt.show()
+            filename = "STFT_SX4_%s_%d" % (self.date, self.shotnum)
+
+        if(IForMPorSX=="REF"):
+            SX_FAST = self.load_SX_FAST("PPL")
+            y = SX_FAST[3:, :]
+            x = SX_FAST[0, :]
+            filename = "STFT_REF_%s_%d" % (self.date, self.shotnum)
+            vmin = 0.0
+            vmax = 5e-7
+            NPERSEG = 2**14
+            time_offset_stft = 0.75
+            time_offset = 1.25
+            #plt.plot(x, SX_FAST[3, :]+1, label="REF_COS")
+            #plt.plot(x, SX_FAST[4, :], label="REF_SIN")
+            #plt.plot(SX_FAST[3, ::10000], SX_FAST[4, ::10000])
+            #plt.legend()
+            #plt.show()
+
+
+        N = np.abs(1/(x[1]-x[2]))
+
+        for i in range(num_ch):
+
+            f, t, Zxx =sig.spectrogram(y[i, :], fs=N, window='hamming', nperseg=NPERSEG)
+            if(i == 0):
+                Zxx_3D = np.zeros((np.shape(Zxx)[0], np.shape(Zxx)[1], num_ch))
+            Zxx_3D[:, :, i] = Zxx[:, :]
+
+        return f, t, Zxx_3D, filename, vmax, vmin, time_offset, time_offset_stft, x, y
+
+    def plot_stft(self, IForMPorSX="IF", num_ch=4):
+        f, t, Zxx_3D, filename, vmax, vmin, time_offset, time_offset_stft, x, y = self.stft(IForMPorSX=IForMPorSX, num_ch=num_ch)
+
+        plt.figure(figsize=(16, 5))
+        gs = gridspec.GridSpec(4, num_ch)
+        gs.update(hspace=0.4, wspace=0.3)
+        for i in range(num_ch):
+            ax0 = plt.subplot(gs[0:3, i])
+            plt.pcolormesh(t + time_offset_stft, f, np.abs(Zxx_3D[:, :, i]), vmin=vmin, vmax=vmax)
+            sfmt=matplotlib.ticker.ScalarFormatter(useMathText=True)
+            cbar = plt.colorbar(format=sfmt)
+            cbar.ax.tick_params(labelsize=12)
+            cbar.formatter.set_powerlimits((0, 0))
+            cbar.update_ticks()
+            ax0.set_xlabel("Time [sec]")
+            ax0.set_ylabel("Frequency of %s%d [Hz]" % (IForMPorSX, i+1))
+            ax0.set_xlim(0.5, 2.5)
+            ax0.set_ylim([0, 2000])
+            if(i==num_ch-1):
+                plt.title("%s" % (filename), loc='right', fontsize=20, fontname="Times New Roman")
+
+            ax1 = plt.subplot(gs[3, i])
+            ax1.plot(x+time_offset, y[i, :])
+            ax1.set_xlabel("Time [sec]")
+            ax1.set_xlim(0.5, 3.0)
+        plt.show()
+        #filepath = "figure/"
+        #plt.savefig(filepath + filename)
+
+def make_stft_profile(date):
+    r_pol = np.array([379, 432, 484, 535, 583, 630, 689, 745, 820])
+    num_shots = np.array([87, 54, 89, 91, 93, 95, 97, 99, 101])
+
+    for i in range(9):
+        stft = STFT_RT1(date=date, shotNo=num_shots[i], LOCALorPPL="PPL")
+        f, t, Zxx_3D,_,_,_,_,_,_,_ = stft.stft(IForMPorSX="POL", num_ch=4)
+        if(i == 0):
+            Zxx_4D = np.zeros((np.shape(Zxx_3D)[0], np.shape(Zxx_3D)[1], np.shape(Zxx_3D)[2], r_pol.__len__()))
+        Zxx_4D[:, :, :, i] = Zxx_3D
+
+    filename = 'Pol_stft_%s_%dto%d.npz' % (date, num_shots[0], num_shots[-1])
+    np.savez_compressed(filename, r_pol=r_pol, f=f, t=t, Zxx_4D=Zxx_4D)
+
+
+if __name__ == "__main__":
+    #for i in range(47, 87):
+    #    stft = STFT_RT1(date="20180223", shotNo=i, LOCALorPPL="PPL")
+    #    stft.stft(IForMPorSX="POL", num_ch=4)
+    #stft = STFT_RT1(date="20180223", shotNo=47, LOCALorPPL="PPL")
+    #stft.plot_stft(IForMPorSX="POL", num_ch=4)
+    make_stft_profile(date="20180223")
+    #stft.cwt()
+    #stft.cross_spectrum()
